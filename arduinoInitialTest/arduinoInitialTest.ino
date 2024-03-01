e5c7373e5a8673790f187c30a990b56acd48fce6
falue <info@fluescher.ch>
2024-03-01 16:02:03 +0100

    Add ground truth; some butchering of everything

--- arduinoInitialTest/arduinoInitialTest.ino
+++ arduinoInitialTest/arduinoInitialTest.ino
@@ -22,16 +22,17 @@
 
   SparkFun code, firmware, and software is released under the MIT License(http://opensource.org/licenses/MIT).
 */
+#include <Adafruit_NeoPixel.h>  // onboard LED
+#define LED_PIN    40
+#define LED_COUNT 1
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 #include "SparkFun_TMF882X_Library.h" //http://librarymanager/All#SparkFun_Qwiic_TMPF882X
 
 SparkFun_TMF882X  myTMF882X;
 
-// IC2 for esp32
-#include <Wire.h>
-#define SDA_PIN 21
-#define SCL_PIN 22
-
 // Structure to hold the measurement results - this is defined by the TMF882X SDK.
+
 static struct tmf882x_msg_meas_results myResults;
 
 int distanceStartDimMm = 3000;  // mm distance from where it should start to fade out from maxDimValue (fully clear)
@@ -40,11 +41,11 @@ int maxDimValue = 255;  // if furthest, this value is used
 
 // Leave me alone
 const int numSensors = 9;
-const int numMeasurements = 6;
-int rollingAverages[numSensors] = {0}; // 1D array for rolling averages
+const int numMeasurements = 3;
+long rollingAverages[numSensors] = {0}; // 1D array for rolling averages
 int groundTruth[numSensors] = {5000}; // 1D array for rolling averages
 int deviation[numSensors] = {0}; // 1D array for differences between measurement, groundTruth
-int sensorValues[numSensors][numMeasurements] = {0}; // 2D array to store sensor values
+long sensorValues[numSensors][numMeasurements] = {0}; // 2D array to store sensor values
 int deadZone = 100;  // +/- allowed noise/deviation
 int confidenceBooster = 1;  // boost confidence in measurement. 1 = not so sure (slowly changing values), 2 = twice as sure (faster changing), etc
 
@@ -64,32 +65,12 @@ but then it's not better than a normal UV sensor.
 
 */
 
-
-
-
-
-/*
-
-
-
-
-!!!!
-     
-     THIS SCRIPT IS BUTCHERED
-
-     WITHOUT NEOPIXEL LEDS BUT TRYING TO GET IC2 / TMF8820 TO WORK ON ESP32
-
-     FOR ARDUINO / CODE BASE CHECKOUT THE arduinoInitialTest FOLDER
-
-!!!!
-
-
-
-
-*/
-
 void setup() {
-    Wire.begin(SDA_PIN, SCL_PIN);  // esp32 IC2 comms
+    strip.begin();
+    strip.show();
+    strip.setPixelColor(0, 0, 0, 255);
+    strip.show();
+    
     delay(1000);
     Serial.begin(115200);
     Serial.println("");
@@ -98,10 +79,8 @@ void setup() {
 
     // Initialize the TMF882X device
     if(!myTMF882X.begin()) {
-        while(1) {
-          Serial.println("Error - The TMF882X failed to initialize - is the board connected?");
-          delay(100);
-        };
+        Serial.println("Error - The TMF882X failed to initialize - is the board connected?");
+        while(1);
     } else {
       Serial.println("TMF882X started.");
     }
@@ -109,6 +88,8 @@ void setup() {
 
     // Establish ground truth (measure room)
     for (int i = 0; i < 100; ++i) {
+      strip.setPixelColor(0, 255, 255, 255);
+      strip.show();
       if(myTMF882X.startMeasuring(myResults)) {
           for (int i = 0; i < myResults.num_results; ++i) {
               updateRollingAverage(true, myResults.results[i].channel-1, myResults.results[i].distance_mm);
@@ -119,18 +100,26 @@ void setup() {
       Serial.print(i);
       Serial.print("%..\t");
       printAverages(groundTruth);
+      strip.setPixelColor(0, 0, 0, 255);
+      strip.show();
       delay(75);
     }
 
     // Set current sensor values to ground truth
     // memcpy(sensorValues, groundTruth, sizeof(sensorValues)); ==> makes garbage
 
+    strip.setPixelColor(0, 0, 255, 0);
+    strip.show();
     Serial.println("==============================");
     Serial.println("Calibration finished!");
     Serial.println("==============================");
 }
 
 void loop() {
+
+    Serial.print(2500);
+    Serial.print("\t");
+
     // get a Measurement
     if(myTMF882X.startMeasuring(myResults)) {
         // print out results
@@ -143,8 +132,8 @@ void loop() {
             // Serial.print(" distance mm: "); Serial.print(myResults.results[i].distance_mm);
             // Serial.print(" channel: "); Serial.print(myResults.results[i].channel);
             // Serial.print(" sub_capture: "); Serial.println(myResults.results[i].sub_capture);
-            // updateRollingAverage(false, myResults.results[i].channel-1, myResults.results[i].distance_mm);
-            updateRollingAverageWithConfidence(false, myResults.results[i].channel-1, myResults.results[i].confidence  *  confidenceBooster , myResults.results[i].distance_mm);
+            updateRollingAverage(false, myResults.results[i].channel-1, myResults.results[i].distance_mm);
+            // updateRollingAverageWithConfidence(false, myResults.results[i].channel-1, myResults.results[i].confidence  *  confidenceBooster , myResults.results[i].distance_mm);
         }
         // Serial.print("     photon: "); Serial.print(myResults.photon_count);    
         // Serial.print(" ref photon: "); Serial.print(myResults.ref_photon_count);
@@ -164,10 +153,17 @@ void loop() {
       Serial.print(switchFoil);
       Serial.println();*/
 
+      strip.setPixelColor(0, switchFoil, 0, 0);
+      // strip.setPixelColor(0, 255, 0, 0);
+      strip.show();
+
     } else {
       Serial.println("");
+      strip.setPixelColor(0, 0, 255, 0);
+      strip.show();
     }
 
+    
     delay(15);
 }
 
