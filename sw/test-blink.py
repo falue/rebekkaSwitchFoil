a32d41b5c2144ffde84bbe77c822d4c60e413e6f
falue <info@fluescher.ch>
2024-06-14 15:15:52 +0200

    Add LED blink script

---
+++ sw/test-blink.py
@@ -0,0 +1,20 @@
+from machine import Pin
+import time
+
+print("System started...")
+
+# Define the pin connected to the LED
+led_pin = Pin(13, Pin.OUT)  # D13 on SparkFun RedBoard
+
+# Function to turn the LED on and off
+def blink_led():
+    while True:
+        led_pin.value(1)  # Turn the LED on
+        print('LED ON')
+        time.sleep(1)     # Wait for 1 second
+        led_pin.value(0)  # Turn the LED off
+        print('LED OFF')
+        time.sleep(1)     # Wait for 1 second
+
+# Call the function
+blink_led(10)
