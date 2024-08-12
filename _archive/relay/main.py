b4f7475c5e4c35e0f01ff0a5147d1451f24935c6
falue <info@fluescher.ch>
2024-08-12 09:08:46 +0200

    Make better for debugging

--- _archive/relay/main.py
+++ _archive/relay/main.py
@@ -4,9 +4,9 @@ import time
 print("System started... B")
 
 # Define the pin connected to the relay
-led_pin = Pin(13, Pin.OUT)    # D13 on SparkFun RedBoard
-relay_pin = Pin(4, Pin.OUT)  # D19 on SparkFun RedBoard 4=A5   MANUAL WIRING !!!!
-relay_pin = Pin(18, Pin.OUT)  # D19 on SparkFun RedBoard 4=A5  PCB !!!!
+led_pin = Pin(13, Pin.OUT)    # D13 on SparkFun Thing Plus
+relay_pin = Pin(4, Pin.OUT)  # D19 on SparkFun Thing Plus 4=A5   MANUAL WIRING !!!!
+#relay_pin = Pin(18, Pin.OUT)  # PCB !!!!
 
 # Function to turn the relay on and off
 def switch_relay(num_iterations):
@@ -21,12 +21,12 @@ def blink():
     relay_pin.value(1)  # Turn the relay on
     led_pin.value(1)    # Turn the LED on
     print('relay ON')
-    time.sleep(2)       # Wait for 1 second
+    time.sleep(4)       # Wait for 4 seconds
 
     relay_pin.value(0)  # Turn the relay off
     led_pin.value(0)    # Turn the LED off
     print('relay OFF')
-    time.sleep(2)       # Wait for 1 second
+    time.sleep(4)       # Wait for 4 seconds
 
 
 # Call the function
